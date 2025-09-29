841001d0a7173e31ba6c95341adf313604ea787d
Gregory <gregorynoelmarcellosoetanto_2327240045@mhs.mdp.ac.id>
2025-09-29 16:55:26 +0700

    pertemuan 6 (unfinished)

--- toko-online/app.js
+++ toko-online/app.js
@@ -4,14 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+//perbaikan 2
+var indexRouter = require('./app_toko_online/routes/index');
+var usersRouter = require('./app_toko_online/routes/users');
+var productRouter = require('./app_toko_online/routes/product'); //import route product
 var engine = require('ejs-blocks'); // menggunakan ejs block
 var app = express();
-var productRouter = require('./routes/product');//tambahkan router dari product.js
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'app_toko_online', 'views')); // perbaikan 1
 app.engine('ejs',engine); //daftarkan engine ejs-block
 app.set('view engine', 'ejs');
 
